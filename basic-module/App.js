254544a5f7b27dc0307b4508a23e70fc8be58b9a
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-08-03 00:10:10 -0300

    using states in class component

--- basic-module/App.js
+++ basic-module/App.js
@@ -1,35 +1,28 @@
 import React, { Component } from "react";
-import { View, Text, Image } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: 'Matheus'
+    }
+    this.entrar = this.entrar.bind(this)
+  }
+
+  entrar(){
+    this.setState({
+      nome: 'Jose Silva'
+    })
+  }
+
   render() {
     let nome = 'Ryan'
     return (
       <View>
-        <Text>Olá Mundo!</Text>
-        <Text style={{ color: '#123', fontSize: 25 }}>Olá DeBoa?!</Text>
-
-        <Jobs largura={200} altura={400} nome='Ryan' />
-
-        <Text>{nome}</Text>
-      </View>
-    )
-  }
-}
-
-class Jobs extends Component {
-  render() {
-    return (
-      <View>
-        <Image
-          source={{
-            uri: 'https://sujeitoprogramador.com/steve.png'
-          }}
-          style={{
-            width: this.props.largura,
-            height: this.props.altura
-          }} />
-        <Text>{this.props.nome}</Text>
+        <Text>{this.state.nome}</Text>
+        <Button title="Trocar" onPress={this.entrar}/>
       </View>
     )
   }
