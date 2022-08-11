1b68daf73b3fcaf992250cc519f4e03927dcf747
Ryan Gualberto <RyanOliveiraGualberto@gmail.com>
2022-08-11 00:16:29 -0300

    add clone insta

---
+++ using/App.js
@@ -0,0 +1,26 @@
+import react, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
