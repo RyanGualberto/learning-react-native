3759000c9672694a8be77bb52f493d61a56d22e1
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-08-15 23:04:51 -0300

    learning more stack navigation

--- working-navigation/App.js
+++ working-navigation/App.js
@@ -1,34 +1,45 @@
-import { NavigationContainer } from "@react-navigation/native";
+import React from 'react';
 
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from "./src/pages/Home";
-import Sobre from "./src/pages/Sobre";
+import Home from './src/pages/Home';
+import Sobre from './src/pages/Sobre';
+import Contato from './src/pages/Contato';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-  return (
+export default function App(){
+  return(
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
+        <Stack.Screen 
+        name="Home" 
+        component={Home} 
+        options={{
+          title: 'Tela inicio',
+          headerStyle:{
+            backgroundColor: '#121212'
+          },
+
+          headerTintColor: '#fff',
+          headerShown: false,
+        }}
         />
-        <Stack.Screen
-          name="Sobre"
-          component={Sobre}
-          options={{
-            title: "Welcome to Sobre",
-            headerStyle: {
-              backgroundColor: "#121212",
-            },
-            headerTintColor: "#fff",
-          }}
+
+        <Stack.Screen 
+        name="Sobre" 
+        component={Sobre} 
+        options={{
+          title:'Pagina Sobre'
+        }}
+        />
+
+        <Stack.Screen 
+        name="Contato"
+        component={Contato}
         />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
--- working-navigation/src/pages/Home/index.js
+++ working-navigation/src/pages/Home/index.js
@@ -1,24 +1,27 @@
-import React from "react";
-import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-export default function Home(props) {
+import { useNavigation } from '@react-navigation/native';
+
+export default function Home(){
   const navigation = useNavigation();
-  return (
+
+  function navegaSobre(){
+    navigation.navigate('Sobre', {  nome: 'Lucas Silva', email: 'matheus@teste.com' })
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Tela Home</Text>
-      <TouchableOpacity>
-        <Text onPress={() => navigation.navigate("Sobre")}>Trocar Tela</Text>
-      </TouchableOpacity>
+      <Text>Tela HOME</Text>
+      <Button title="Ir para sobre" onPress={ navegaSobre } />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ddd",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+  container:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
