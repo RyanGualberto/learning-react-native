0e1cacfe70a16b918e6fdf087f20ba5bc8dac1f6
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-08-15 23:57:51 -0300

    mix navigators

---
+++ working-tab-navigation/src/pages/Detalhes/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+export default function App(props) {
+  return (
+    <View style={styles.container}>
+      <Text>Pagina Detalhes</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ddd",
+  },
+});
--- working-tab-navigation/src/pages/Home/index.js
+++ working-tab-navigation/src/pages/Home/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Home(props) {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Text>Rela Home</Text>
+      <Text>Tela Home</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Details")}>
+        <Text>Ir</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,5 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ddd",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
