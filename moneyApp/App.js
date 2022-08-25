c77422425f11f62fa315482eabe16b0a31e44d4b
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-08-25 11:50:50 -0300

    init project

---
+++ moneyApp/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import {Container, Titulo} from './src/styles';
+
+export default function App(props) {
+  return (
+    <Container>
+      <Titulo>Seja bem Vindo</Titulo>
+    </Container>
+  );
+}
---
+++ moneyApp/src/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #ff0000;
+`;
+
+export const Titulo = styled.Text`
+  color: white;
+  font-size: 25px;
+`;
